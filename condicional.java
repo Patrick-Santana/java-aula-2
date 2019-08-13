3f431e4d5cdea60b8140630ef8d4fe1cff6f768f
Star2k19 <zonaabissal2016@gmail.com>
2019-08-13 10:28:55 -0300

    Create condicional.java

---
+++ condicional.java
@@ -0,0 +1,16 @@
+public class condicional {
+
+	     public static void main(String[] args) {
+	     	               
+	     	               int cont = 1;
+
+	     	               while (cont <= 30){
+
+	     	               	System.out.println(cont);
+	     	               	cont= cont + 2;
+
+	     	               }
+
+	     }
+
+}
